1f371ab4ec5b3f8f64790ede4b8fc25f3d5d107c
Ajay-10D <141933815+Ajay-10D@users.noreply.github.com>
2023-11-28 12:46:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import RegisterForm from "./components/Register";
+import BlogPostForm from "./components/createBlog";
+import HomePage from "./components/homePage";
+import InspectBlog from "./components/inspectBlog";
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
